710045b91d622fd03a6723ac6642e918f44b58fe
sofia natalizi <sofia.natalizi@gmail.com>
2026-06-06 15:47:41 +0200

    modifica tabella

--- dataset_project.R
+++ dataset_project.R
@@ -2,4 +2,11 @@ library(here)
 
 dati_progetto <- read.csv (here("dataset.csv"))
 
+
+dati_progetto$Blood.Pressure <- NULL
+dati_progetto$Person.ID <- NULL
+dati_progetto$Sleep.Disorder <- NULL
+
 head(dati_progetto)
+
+#> DT::datatable(dati_progetto)
