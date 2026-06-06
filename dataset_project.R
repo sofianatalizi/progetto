5f98342153b2fd54446e3db0a6795746d585a627
momsim6 <mombelli.simone04@gmail.com>
2026-06-06 15:52:59 +0200

    grafici

--- dataset_project.R
+++ dataset_project.R
@@ -6,7 +6,12 @@ dati_progetto <- read.csv (here("dataset.csv"))
 dati_progetto$Blood.Pressure <- NULL
 dati_progetto$Person.ID <- NULL
 dati_progetto$Sleep.Disorder <- NULL
-
 head(dati_progetto)
-
+pairs(dati_progetto[ , c('Age', 'Sleep.Duration', 'Quality.of.Sleep', 'Physical.Activity.Level',
+                          'Stress.Level','Heart.Rate', 'Daily.Steps')], pch = 16)
+ggpairs(data = dati_progetto, title ="Relationships between predictors & response", 
+        lower = list(continuous=wrap("points", alpha = 0.5, size=0.1)))
+g = lm( Quality.of.Sleep ~ Age + Sleep.Duration + Physical.Activity.Level +
+          Stress.Level + Heart.Rate+ Daily.Steps, data = dati_progetto )
+summary(g)
 #> DT::datatable(dati_progetto)
